c531957e0d25f4a5dfdbae78cb48cfea32a7f4f5
benlayer291 <benlayer291@gmail.com>
2015-10-31 16:23:33 +0000

    changed root path to be sign in page if not logged in, otherwise goes to user#show page

--- prep_app/app/controllers/application_controller.rb
+++ prep_app/app/controllers/application_controller.rb
@@ -13,10 +13,6 @@ class ApplicationController < ActionController::Base
         devise_parameter_sanitizer.for(:sign_up) do |u|
           u.permit(:first_name, :second_name, :role, :cohort_id, :image, :email, :password, :password_confirmation)
         end
-
-        devise_parameter_sanitizer.for(:account_update) do |u|
-          u.permit(:first_name, :second_name, :role, :cohort_id, :image, :email, :password, :password_confirmation, :current_password)
-        end
       end
   end
 
--- prep_app/config/routes.rb
+++ prep_app/config/routes.rb
@@ -1,11 +1,24 @@
 Rails.application.routes.draw do
 
-  root "users#show"
+
+
+  # root "users#show"
   # devise_scope :user do
+  #   # if current_user
+  #   # root to: "users#show"
+  #   # else
   #   root to: "devise/sessions#new"
   # end
 
   devise_for :users
   resources :users, only: [:show]
+
+  authenticated :user do
+    root "users#show", as: :authenticated_root
+  end
+
+  devise_scope :user do
+      root to: "devise/sessions#new"
+  end
 
 end
