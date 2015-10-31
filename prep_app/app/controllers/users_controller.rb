fba2fe9d0bd0e9b8e9642fb2dd2e310086daef45
benlayer291 <benlayer291@gmail.com>
2015-10-31 20:19:53 +0000

    Updated users controller with 7 restful routes. Will be useful for admin role

--- prep_app/app/controllers/users_controller.rb
+++ prep_app/app/controllers/users_controller.rb
@@ -1,11 +1,57 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
+  #Get
   def index 
-    @users= User.all
+    @users = User.all
   end
-  
+
+  #Get
   def show
-    @user= current_user
+  end
+
+  #Get
+  def new
+    @user = User.new
+  end
+
+  #Get
+  def edit
+  end
+
+  #Post
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  #Put
+  def update
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  #Delete
+
+  def destroy
+    @user.destroy
+    redirect_to users_path
+  end
+
+  def set_post
+    @user = User.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:first_name, :second_name, :role, :cohort_id, :image, :email, :password, :password_confirmation)
   end
 
 end
