eb21afc8671276f164112fc8af855ec84e3d8c9c
benlayer291 <benlayer291@gmail.com>
2015-10-31 15:37:38 +0000

    Added register button so as to aid initial testing

--- prep_app/config/routes.rb
+++ prep_app/config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
-  devise_scope :user do
-    root to: "devise/sessions#new"
-  end
+  root "users#show"
+  # devise_scope :user do
+  #   root to: "devise/sessions#new"
+  # end
 
   devise_for :users
   resources :users, only: [:show]
-  
+
 end
