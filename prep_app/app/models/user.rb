1bc9cd57c8c311f838f491ae47dec44feb5bdf7f
benlayer291 <benlayer291@gmail.com>
2015-10-31 20:27:08 +0000

    Updated associations for user model with has_many_as_teacher and has_and_belongs_to_many

--- prep_app/app/models/user.rb
+++ prep_app/app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :cohorts_as_teacher, class_name: "User", foreign_key: "teacher_id"
+  has_and_belongs_to_many :cohorts 
+
 end
