2ac6e423fd429a15a882e738e65a9f85373fd0df
benlayer291 <benlayer291@gmail.com>
2015-11-02 09:23:16 +0000

    Monday morning, created cohorts_users join table

--- prep_app/db/schema.rb
+++ prep_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151031182607) do
+ActiveRecord::Schema.define(version: 20151102092219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(version: 20151031182607) do
     t.integer  "teacher_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "cohorts_users", id: false, force: :cascade do |t|
+    t.integer "cohort_id", null: false
+    t.integer "user_id",   null: false
   end
 
   create_table "users", force: :cascade do |t|
