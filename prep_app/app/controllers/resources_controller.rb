28480c44b643bf8f1311df73ce68c404de09b369
benlayer291 <benlayer291@gmail.com>
2015-11-02 11:02:48 +0000

    Update resources controller with seven restful routes

--- prep_app/app/controllers/resources_controller.rb
+++ prep_app/app/controllers/resources_controller.rb
@@ -1,22 +1,47 @@
 class ResourcesController < ApplicationController
+  before_action :set_resource, only: [:show, :edit, :update, :destroy]
+
   def index
+    @resources = Resource.all
   end
 
   def show
   end
 
   def new
+    @resource = Resource.new
   end
 
   def create
+    @resource = Resource.new(resource_params)
+    if @resource.save
+      redirect_to @resource
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @resource.update(resource_params)
+      redirect_to @resource
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @resource.destroy
+    redirect_to resources_path
+  end
+
+  def set_resource
+    @resource = Resource.find(params[:id])
+  end
+
+  def resource_params
+    params.require(:resource).permit(:name, :description, :file, :image, :cohort_id)
   end
 end
