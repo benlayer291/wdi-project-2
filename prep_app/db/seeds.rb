b8f3c01b2f70d102908eb585481dbd5e129a7f47
benlayer291 <benlayer291@gmail.com>
2015-11-02 11:52:40 +0000

    Created some seed data for cohorts and resources in seed file. Now going to try and seed database

--- prep_app/db/seeds.rb
+++ prep_app/db/seeds.rb
@@ -8,13 +8,26 @@
 
 # User(id: integer, email: string, first_name: string, second_name: string, image: string, role: string, cohort_id: integer)
 
+#currently by registering...
+
 # Cohort(id: integer, name: string, teacher_id: integer, created_at: datetime, updated_at: datetime)
 
-cohort1 = Cohort.create(name: 'SS-CH1', teacher_id: 1)
-cohort2 = Cohort.create(name: 'SS-CH2', teacher_id: 2)
-cohort3 = Cohort.create(name: 'SS-CH3', teacher_id: 3)
-cohort4 = Cohort.create(name: 'SS-CH4', teacher_id: 4)
-cohort5 = Cohort.create(name: 'SS-CH5', teacher_id: 5)
-cohort6 = Cohort.create(name: 'SS-CH6', teacher_id: 6)
+c1 = Cohort.create!(name: 'SS-CH1', teacher_id: 2)
+c2 = Cohort.create!(name: 'SS-CH2', teacher_id: 2)
+c3 = Cohort.create!(name: 'SS-CH3', teacher_id: 2)
+c4 = Cohort.create!(name: 'SS-CH4', teacher_id: 2)
+c5 = Cohort.create!(name: 'SS-CH5', teacher_id: 2)
+c6 = Cohort.create!(name: 'SS-CH6', teacher_id: 2)
+
+#Resource(id: integer, name: string, description: text, image: string, file: string, cohort_id: integer)
+
+r1 = c1.resources.create!(name: 'Homework 1', description: 'Complete all questions on the worksheet. Write your name and the date at the top of the page. You will need to draw a graph for question 3 which you should do using a pencil and ruler on graph paper', image: 'http://www.stmarysrcmidd.rochdale.sch.uk/files/images/keep-calm-and-do-that-homework.png', file: "file path")
+r2 = c2.resources.create!(name: 'Homework 2', description: 'Complete all questions on the worksheet. Write your name and the date at the top of the page. You will need to draw a graph for question 3 which you should do using a pencil and ruler on graph paper', image: 'http://www.stmarysrcmidd.rochdale.sch.uk/files/images/keep-calm-and-do-that-homework.png', file: "file path")
+r3 = c3.resources.create!(name: 'Homework 3', description: 'Complete all questions on the worksheet. Write your name and the date at the top of the page. You will need to draw a graph for question 3 which you should do using a pencil and ruler on graph paper', image: 'http://www.stmarysrcmidd.rochdale.sch.uk/files/images/keep-calm-and-do-that-homework.png', file: "file path")
+r4 = c4.resources.create!(name: 'Homework 4', description: 'Complete all questions on the worksheet. Write your name and the date at the top of the page. You will need to draw a graph for question 3 which you should do using a pencil and ruler on graph paper', image: 'http://www.stmarysrcmidd.rochdale.sch.uk/files/images/keep-calm-and-do-that-homework.png', file: "file path")
+r5 = c5.resources.create!(name: 'Homework 5', description: 'Complete all questions on the worksheet. Write your name and the date at the top of the page. You will need to draw a graph for question 3 which you should do using a pencil and ruler on graph paper', image: 'http://www.stmarysrcmidd.rochdale.sch.uk/files/images/keep-calm-and-do-that-homework.png', file: "file path")
+r6 = c6.resources.create!(name: 'Homework 6', description: 'Complete all questions on the worksheet. Write your name and the date at the top of the page. You will need to draw a graph for question 3 which you should do using a pencil and ruler on graph paper', image: 'http://www.stmarysrcmidd.rochdale.sch.uk/files/images/keep-calm-and-do-that-homework.png', file: "file path")
+
+# Setup associations in Join Table- Cohorts-Users
 
 
