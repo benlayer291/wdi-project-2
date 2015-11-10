7555e7a7e32860277ea5b34143290b7f8e07a21e
benlayer291 <benlayer291@gmail.com>
2015-11-10 18:54:31 +0000

    Adding dependent destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :cohorts_as_teacher, class_name: "User", foreign_key: "teacher_id"
+  has_many :cohorts_as_teacher, class_name: "User", foreign_key: "teacher_id", dependent: :destroy
   has_and_belongs_to_many :cohorts 
 
 end
